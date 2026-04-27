6e1ef5e81ece3f0af5a0e7f72ffc89663132d932
Haowen Yu <elvisyuovo@gmail.com>
2026-04-26 23:19:46 -0400

    Initial commit

---
+++ configs/sections/training.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+from configs.config_tracker import TrackedConfigMixin
+
+
+@dataclass
+class TrainingConfig(TrackedConfigMixin):
+    deterministic: bool = False
+    use_compile: bool = False
+    inference_mode: bool = False
+    seed: int = 42
+    max_epochs: int = 1
+    num_sanity_val_steps: int = 4
